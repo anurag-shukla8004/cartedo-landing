e0e9895b680d388fc17ae041ba6625d65fd3c63b
Ashukla231 <AShukla@archsystemsinc.com>
2026-07-02 23:28:31 +0530

    live-action

--- cartedo-track-config.js
+++ cartedo-track-config.js
@@ -2,4 +2,4 @@
  * ScaleMargin backend public URL for /track/open and /track/click.
  * Update when cloudflared/devtunnel URL changes, then redeploy Netlify.
  */
-window.CARTEDO_TRACK_API = "https://heater-creating-jets-pmc.trycloudflare.com";
+window.CARTEDO_TRACK_API = "https://evolution-denied-ecology-cincinnati.trycloudflare.com";
